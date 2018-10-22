3e3eeb558ff28358584825b458f5481059fce066
Balkon <44376363+balkonafide@users.noreply.github.com>
2018-10-22 19:57:10 +0200

    Update hausaufgabe.py

---
+++ hausaufgabe.py
@@ -0,0 +1,51 @@
+class SmartDevice(object):
+    pass
+# Smart devices class
+
+class Apple(SmartDevice):
+    pass
+
+
+class Samsung(SmartDevice):
+    pass
+
+
+class Xiaomi(SmartDevice):
+    pass
+
+
+class iPhone(Apple)
+    pass
+
+
+class iPad(Apple)
+    pass
+
+
+class MacBook(Apple)
+    pass
+
+
+class GalaxyPhone(Samsung)
+    pass
+
+
+class GalaxyTab(Samsung)
+    pass
+
+
+class GakaxyPC(Samsung)
+    pass
+
+
+class XiaomiPhone(Xiaomi)
+    pass
+
+
+class XiaomiTab(Xiaomi)
+    pass
+
+
+class XiaomiAirFilter(Xiaomi)
+    pass
+#PiotrB
